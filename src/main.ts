b0a51cf609ae65f98e795ae79abf6fb44069a7ad
softxbdsaas <softxbdsaas@gmail.com>
2024-09-12 09:08:25 +0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  /*
+   * Use validation pipes globally
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
+}
+bootstrap();
